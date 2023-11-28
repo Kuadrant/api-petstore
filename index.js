5e303baac3babce56560d678f382deb23a0e1bd3
Jason Madigan <jason@jasonmadigan.com>
2023-11-28 09:23:23 +0000

    flags

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const swaggerUi = require('swagger-ui-express');
 const fs = require("fs");
 const YAML = require('yaml');
+const path = require('path');
 
 require('dotenv').config();
 
@@ -10,12 +11,13 @@ require('dotenv').config();
 const port = 8080;
 
 app.use(express.json());
+app.use('/static', express.static('static'));
 
 const file = fs.readFileSync('./openapi.yaml', 'utf8')
 const swaggerDocument = YAML.parse(file)
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
-app.get('/', function (req, res) {
+app.get('/', function(req, res) {
   res.redirect('/docs');
 });
 
@@ -27,9 +29,32 @@ let pets = [
 app.get('/server/details', function (req, res) {
   const region = process.env.REGION;
   if (region) {
-      res.send(`REGION: ${region}`);
+    // Construct the path to the SVG file based on the region.
+    const svgFilePath = `/static/${region}.svg`; // Make sure the path matches your static files setup.
+
+    const htmlResponse = `
+      <!DOCTYPE html>
+      <html>
+      <head>
+        <title>Server Details</title>
+        <style>
+          body, html {
+            margin: 0;
+            padding: 0;
+            height: 100%;
+            background: url('${svgFilePath}') no-repeat center center fixed;
+            background-size: cover; /* Cover the entire viewport */
+          }
+        </style>
+      </head>
+      <body>
+        <!-- The body background image is now the SVG -->
+      </body>
+      </html>
+    `;
+    res.send(htmlResponse);
   } else {
-      res.status(404).send('REGION environment variable not found.');
+    res.status(404).send('REGION environment variable not found.');
   }
 });
 
@@ -64,6 +89,6 @@ app.listen(port, () => {
 });
 
 process.on('SIGINT', function() {
-  console.log( "\nGracefully shutting down from SIGINT (Ctrl-C)" );
+  console.log("\nGracefully shutting down from SIGINT (Ctrl-C)");
   process.exit(1);
 });
